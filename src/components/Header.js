2935852fbf8308a67561ed7c6b6925086520df62
trisjones <trisjones04@gmail.com>
2018-10-10 10:56:55 -0400

    Had to delete repo and restart due to API key security issue. Reinstating project in this repo and will resolve API key security going forward.

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../App.css';
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <p>BucketList</p>
+    </header>
+  )
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+const defaultState = {
+  markers: [],
+  selectedMarker: {},
+}
+
+const reducer = (state=defaultState, action) => {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
+
+export default reducer
